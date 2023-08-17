12a5ab81aa02aa162459efe5269c9a17426c0a53
elieldiniz <elielfantichelle@outlook.com.br>
2023-08-17 11:19:12 -0400

    frish commit

---
+++ src/components/Carrosel.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+export default function Carrosel(){
+
+    const Slide = [
+        {
+            url: 'drone1.svg'
+        },
+        {
+            url: 'drone2.svg'
+        },
+        {
+            url: 'dron3.svg'
+        },
+    ]
+    return (
+        <div>
+           eliel dinzizd dddddd
+        </div>
+    )
+}
